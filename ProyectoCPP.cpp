5df292af71e5367d9ac26d51d8e7e9660a6e54ec
IceCbe <dplana61@icloud-com>
2022-12-17 20:16:47 +0100

    Esquelet Menu

--- ProyectoCPP.cpp
+++ ProyectoCPP.cpp
@@ -1,6 +1,32 @@
 #include "funciones.h"
 
+int menu()
+{
+	int opcion_menu_principal;
+	cout << "Bienvenido." << endl;
+	cout << "1. Fibonacci" << endl << "2. Otros" << endl << "3. Otros" << endl << "4. Salir" << endl;
+	cout << "Selecciona opcion: ";
+	cin >> opcion_menu_principal;
+	while (opcion_menu_principal > 4 || opcion_menu_principal < 1)
+	{
+		cout << endl << "Error. Opcion incorrecta." << endl << "Selecciona opcion: ";
+		cin >> opcion_menu_principal;
+	}
+	return opcion_menu_principal;
+}
+
 int main()
 {
+	int opcion = menu();
+	switch (opcion)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	default:
+		cout << opcion;
+	};
+
 	return 0;
 }
