fb135f086588f61f7396bf16a852bc3a7a7fff79
IceCbe <dplana61@icloud-com>
2022-12-17 20:02:08 +0100

    Commit inicial branca Menu

--- ProyectoCPP.cpp
+++ ProyectoCPP.cpp
@@ -1,6 +1,6 @@
 #include "funciones.h"
+
 int main()
 {
-
 	return 0;
 }
--- funciones.cpp
+++ funciones.cpp
@@ -1,1 +1,15 @@
 #include "funciones.h"
+
+void menu()
+{
+	int opcion_menu_principal;
+	cout << "Bienvenido." << endl;
+	cout << "1. Jugar" << endl << "2. Instrucciones" << endl << "3. Creditos" << endl << "4. Salir" << endl;
+	cout << "Selecciona opcion: ";
+	cin >> opcion_menu_principal;
+	while (opcion_menu_principal > 4 || opcion_menu_principal < 1)
+	{
+		cout << endl << "Error. Opcion incorrecta." << endl << "Selecciona opcion: ";
+		cin >> opcion_menu_principal;
+	}
+}
